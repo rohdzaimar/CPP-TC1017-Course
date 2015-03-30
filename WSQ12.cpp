1c7bd3be52ef6a511bfeaa6895d41409164dbbb9
Rodrigo Hdz <rohdzaimar@gmail.com>
2015-03-30 16:40:07 -0600

    Greatest Common Divisor

---
+++ WSQ12.cpp
@@ -0,0 +1,63 @@
+// The following program calculates the Greatest Common Division between two integers.
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	int gcd;
+	
+	if(a == b)
+	{
+		return a;
+	}
+
+	if (a < b)
+	{
+		int temp;
+
+		temp = a;
+		a = b;
+		b = temp;
+	}
+
+	int rem = a%b;
+
+	if (rem == 0)
+		{
+		return b;
+		}
+
+	while ( rem >= 1)
+	{
+		rem = a % b;
+
+		if (rem == 0)
+		{
+		return b;
+		}
+
+		a = b;
+		b = rem;
+		gcd = b;
+	}
+	return gcd;
+}
+
+int main ()
+{
+	int a;
+	int b;
+
+	cout << "This is a GCD calculator: " << endl;
+	cout << endl;
+	cout << "Enter the first value:" << endl;
+	cin >> a;
+	cout << "Enter the second value:" << endl;
+	cin >> b;
+	cout << endl;
+	cout << "The GCD of " << a << " and " << b << " is: " << gcd(a,b) << endl;
+	return 0;
+}
